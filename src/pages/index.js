4648088efff6e0949ae066672acb6988cbc6f822
Maarten Louage <me@maartenlouage.nl>
2021-08-11 18:10:48 +0200

    Chore

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,22 @@ import { StaticImage } from "gatsby-plugin-image"
 const IndexPage = () => {
   return (
     <>
-      <div class="container">
-        <div>
-          <div className="logo">
-            <StaticImage
-              src="../images/logo.png"
-              alt="Maarten Louage"
-              placeholder="blurred"
-              layout="fixed"
-              width={250}
-              height={250}
-            />
-          </div>
-          <p>
-            Online courses about web development
-          </p>
+      <div className="container">
+        <div className="logo">
+          <StaticImage
+            src="../images/logo.png"
+            alt="Maarten Louage"
+            placeholder="blurred"
+            layout="fixed"
+            width={250}
+            height={250}
+          />
         </div>
+        <p>
+          Webdevelopment <br />
+          &amp; <br />
+          Online courses about webdevelopment
+        </p>
       </div>
     </>
   )

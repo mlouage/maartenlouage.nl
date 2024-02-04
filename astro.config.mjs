693ac41681a355bc15021548aa4442b848471e5d
Maarten Louage <me@maartenlouage.nl>
2024-02-04 15:47:55 +0100

    Add sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    sentry({
+      dsn: "https://9fcd43e03ef354dc9e3160c776d3a16e@o4506689025998848.ingest.sentry.io/4506689030324224",
+      sourceMapsUploadOptions: {
+        project: "maartenlouage-nl",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
+  })],
   output: "server",
   adapter: netlify()
 });

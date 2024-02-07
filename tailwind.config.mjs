0ed95d9a0535bf0b99990eab322598dca1d69b05
Maarten Louage <me@maartenlouage.nl>
2024-02-07 08:20:55 +0100

    Add default components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'ml': {
+					'50': '#f4f9f8',
+					'100': '#d9eeea',
+					'200': '#b4dbd7',
+					'300': '#86c2bc',
+					'400': '#579b97',
+					'500': '#438986',
+					'600': '#346d6c',
+					'700': '#2d5858',
+					'800': '#274848',
+					'900': '#243d3d',
+					'950': '#102323',
+				},
+			}
+		},
 	},
 	plugins: [],
 }

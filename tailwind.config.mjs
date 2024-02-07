12961a7693bd4c9d1591e1658b4750501fe436a4
Maarten Louage <me@maartenlouage.nl>
2024-02-07 19:07:44 +0100

    WIP

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			colors: {
 				'ml': {
-					'50': '#f4f9f8',
-					'100': '#d9eeea',
-					'200': '#b4dbd7',
-					'300': '#86c2bc',
-					'400': '#579b97',
-					'500': '#438986',
-					'600': '#346d6c',
-					'700': '#2d5858',
-					'800': '#274848',
-					'900': '#243d3d',
-					'950': '#102323',
+					'50': 'hsl(168, 29%, 97%)',
+					'100': 'hsl(169, 38%, 89%)',
+					'200': 'hsl(174, 35%, 78%)',
+					'300': 'hsl(174, 33%, 64%)',
+					'400': 'hsl(177, 28%, 50%)',
+					'500': 'hsl(177, 34%, 40%)',
+					'600': 'hsl(179, 35%, 32%)',
+					'700': 'hsl(180, 32%, 26%)',
+					'800': 'hsl(180, 30%, 22%)',
+					'900': 'hsl(180, 26%, 19%)',
+					'950': 'hsl(180, 37%, 10%)',
 				},
+				background: "hsl(var(--background) / <alpha-value>)",
+				primary: "hsl(var(--primary) / <alpha-value>)",
+				'primary-text': "hsl(var(--primary-text) / <alpha-value>)",
+				'primary-hover': "hsl(var(--primary-hover) / <alpha-value>)",
 			}
 		},
 	},

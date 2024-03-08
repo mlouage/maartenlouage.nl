1ab0c8102982d48a39972305130e5b7a1926d6ab
Maarten Louage <me@maartenlouage.nl>
2024-03-08 17:25:31 +0100

    Switch more over to Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 
-import sentry from "@sentry/astro";
-
 export default defineConfig({
   integrations: [
     tailwind(),
     react(),
-    sentry({
-      dsn: "https://9fcd43e03ef354dc9e3160c776d3a16e@o4506689025998848.ingest.sentry.io/4506689030324224",
-      sourceMapsUploadOptions: {
-        project: "maartenlouage-nl",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-    },
-  })],
+  ],
   output: "server",
   adapter: cloudflare()
 });

435d69da06f7bf372c1bfdeec89297420e4e7fc9
Maarten Louage <me@maartenlouage.nl>
2024-03-08 17:12:56 +0100

    Switch over to Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 
 import sentry from "@sentry/astro";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
@@ -18,5 +17,5 @@ export default defineConfig({
     },
   })],
   output: "server",
-  adapter: netlify()
+  adapter: cloudflare()
 });
